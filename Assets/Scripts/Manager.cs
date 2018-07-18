45c940f070a80abae177ae228e87983ff018cbf7
Raphael Kamper <dev@raphiniert.eu>
2018-07-18 22:08:24 +0200

    Chrisi unity expert advised implementation

---
+++ Assets/Scripts/Manager.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Manager : MonoBehaviour {
+    public NoteMarker[] noteMarkers = new NoteMarker[16];
+    public NoteMarker[] activeMarkers = new NoteMarker[16];
+    public NoteMarker[] inactiveMarkers = new NoteMarker[100];
+
+    public LoopController startController;
+    public LoopController endController;
+    public LocationBar locationBar;
+
+
+    public float timeSendOffset = 0f;
+    public int lastSentNote = 0;
+
+	// Use this for initialization
+	void Start () {
+        // Get loop controllers
+        var x = Component.FindObjectsOfType<LoopController>();
+        startController = x[0].startMarker ? x[0] : x[1];
+        endController = x[0].startMarker ? x[1] : x[0];
+
+        locationBar = Component.FindObjectOfType<LocationBar>();
+
+		
+        // Get note markers
+        var markerObjets = GameObject.FindGameObjectsWithTag("Marker");
+        noteMarkers = new NoteMarker[markerObjets.Length];
+        for (int i = 0; i < noteMarkers.Length; ++i) {
+            noteMarkers[i] = markerObjets[i].GetComponent<NoteMarker>();
+        }
+	}
+	
+	// Update is called once per frame
+    void Update () {
+        var tactPosWithOffset = this.locationBar.GetTactPosition(this.locationBar.timeMarker + timeSendOffset);
+
+        if (lastSentNote < tactPosWithOffset || tactPosWithOffset == 0)
+        {
+            lastSentNote = tactPosWithOffset;
+            //Debug.Log(tactPosWithOffset);
+            NoteMarker noteMarker = activeMarkers[lastSentNote];
+            if(noteMarker != null) // TODO: add edge case handling if duration > 1 and tactPostWithOffset == 0
+            {
+                // TODO: Serial Communication with Arduino
+                lastSentNote += noteMarker.duration - 1;
+                Debug.Log("Send note " + this.locationBar.GetNote(noteMarker.transform.position) + " (MarkerID = " + noteMarker.fiducialController.MarkerID + ")");
+            }
+        }
+
+        
+        //var locationBarTactPosition = GetTactPosition(this.locationBar.transform.position);
+        //var noteMarker = this.activeMarkers[locationBarTactPosition];
+	}
+
+
+    public void NoteMarkerMoved(NoteMarker marker, Vector2 delta) {
+        // Remove from active markers
+        for (int i = 0; i < this.activeMarkers.Length; ++i) {
+            if (this.activeMarkers[i] == marker) {
+                this.activeMarkers[i] = null;
+                break;
+            }
+        }
+
+        Debug.Log("Marker " + marker.fiducialController.MarkerID + " moved");
+    }
+
+    public void NoteMarkerRemoved(NoteMarker marker) {// Remove from active markers
+        for (int i = 0; i < this.activeMarkers.Length; ++i)
+        {
+            if (this.activeMarkers[i] == marker)
+            {
+                this.activeMarkers[i] = null;
+                break;
+            }
+        }
+
+        Debug.Log("Marker " + marker.fiducialController.MarkerID + " removed");
+    }
+
+    public void NoteMarkerPositined(NoteMarker marker) {
+        // Add to active markers
+        var tactPos = this.locationBar.GetTactPosition(marker.lastPosition);
+
+        if (tactPos < 0 || tactPos > 15) {
+            return;
+        }
+
+        if (this.activeMarkers[tactPos] != null) {
+            return;
+        }
+
+        this.activeMarkers[tactPos] = marker;
+        Debug.Log("Marker " + marker.fiducialController.MarkerID + " positined (TactPos " + tactPos + ")");
+    }
+}
---
+++ Assets/Scripts/NoteMarker.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoteMarker : MonoBehaviour {
+    public Vector2 lastPosition;
+    private Vector2 threshold = new Vector2(0.5f, 0.5f);
+
+    Manager manager;
+    public int duration = 0;
+
+    public static int startMarkerId = 0;
+    public static int endMarkerId = 31;
+    public static int dvc = 4; // duration variation count
+
+    public FiducialController fiducialController;
+    private float lastTimeMoved;
+    private float lastTimeAlive;
+    // TODO: May depend on BPM
+    private readonly float lastTimeMovedThreshold = 0.2534f;
+
+    // Use this for initialization
+	void Start () {
+        // Init
+        lastPosition = new Vector2(this.transform.position.x, this.transform.position.y);
+        manager = FindObjectOfType<Manager>();
+
+        // Determine the duration
+        fiducialController = this.GetComponent<FiducialController>();
+        var maxNoteCount = endMarkerId - startMarkerId + 1;
+        var markerID = fiducialController.MarkerID;
+        duration = (markerID - startMarkerId) / (maxNoteCount / dvc) + 1; // 1 = 1/4, 2 = 2/4, 3 = 3/4, 4 = 4/4
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (!UniducialLibrary.TuioManager.Instance.IsMarkerAlive(fiducialController.MarkerID)) {
+            if (lastTimeAlive > 0f && Time.time > (lastTimeAlive + lastTimeMovedThreshold)) {
+                manager.NoteMarkerRemoved(this);
+                lastTimeAlive = -1f;
+            }
+            return;
+        }
+
+        // I am alive!
+        lastTimeAlive = Time.time;
+        var currentPosition = new Vector2(this.transform.position.x, this.transform.position.y);
+        var delta = currentPosition - lastPosition;
+
+
+        if (Mathf.Abs(delta.x) < threshold.x && Mathf.Abs(delta.y) < threshold.y) {
+            // No can do babydooll
+            if (lastTimeMoved > 0f && Time.time > (lastTimeMoved + lastTimeMovedThreshold)) {
+                manager.NoteMarkerPositined(this);
+                lastTimeMoved = -1f;
+            }
+            return;
+        }
+
+        // I was moved
+        lastPosition = currentPosition;
+        manager.NoteMarkerMoved(this, delta);
+        lastTimeMoved = Time.time;
+	}
+}
