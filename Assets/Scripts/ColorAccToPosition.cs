111d1c888e86308116ee6538866e089fb2f6d58c
JakobBlattner <Jakoblattner@gmail.com>
2018-08-12 19:05:59 +0200

    added lightly coloured background. marker change their color according to their position.

---
+++ Assets/Scripts/ColorAccToPosition.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorAccToPosition : MonoBehaviour {
+
+    public Color blue;
+    public Color red;
+    public Color green;
+    private Color currentColor;
+
+    private SpriteRenderer rend;
+    private TokenPosition tokenPosition;
+    private float note;
+    //private Vector2 currentPos;
+
+	void Start() {
+        rend = GetComponent<SpriteRenderer>();
+        tokenPosition = TokenPosition.Instance;
+	}
+	
+	void FixedUpdate () {
+        if (rend.isVisible)
+        {
+            note = tokenPosition.GetNote(this.transform.position);
+
+            if (note < 8)
+                rend.color = blue;
+            else if (note < 16)
+                rend.color = green;
+            else
+                rend.color = red;
+
+            currentColor = rend.color;
+        }
+    }
+
+    public Color GetCurrentColor()
+    {
+        return currentColor;
+    }
+}
--- Assets/Scripts/GlowScript.cs
+++ Assets/Scripts/GlowScript.cs
@@ -8,61 +8,53 @@ public class GlowScript : MonoBehaviour
 
     private SpriteRenderer rend;
     private LineRenderer lineRenderer;
+
     private Vector3 lr_pos;
-    private char color;
+    //private char color;
     private Color r_color;
+    private ColorAccToPosition colorAccToPosition;
+
     private Vector3 spriteSize;
     private FiducialController fiducial;
 
-    public Material blueMat;
-    public Material greenMat;
-    public Material redMat;
-    public Material defaultMat;
+    public Material blueMaterial;
+    public Material greenMaterial;
+    public Material redMaterial;
+    public Material defaultMaterial;
 
     void Start()
     {
-        //gets renderer
-        rend = GetComponent<SpriteRenderer>();
-
-        //checks which color the marker has
-        if (rend.color.r > rend.color.b && rend.color.r > rend.color.g)
-            color = 'r';
-        else if (rend.color.g > rend.color.b && rend.color.g > rend.color.r)
-            color = 'g';
-        else
-            color = 'b';
-
         GameObject locationBar = GameObject.Find("Current_Location_Bar");
         lineRenderer = locationBar.GetComponent<LineRenderer>();
 
         fiducial = GetComponent<FiducialController>();
+
+        rend = GetComponent<SpriteRenderer>();
+        colorAccToPosition = GetComponent<ColorAccToPosition>();
         spriteSize = rend.bounds.size;
-
-        r_color = rend.material.color;
-        rend.material = defaultMat;
-        r_color = rend.color;
     }
-    void Update()
+    void FixedUpdate()
     {
-        //gets current position of the current_location_bar
-        lr_pos = lineRenderer.GetPosition(0);
+        if (rend.isVisible)
+        {
+            //gets current position of the current_location_bar
+            lr_pos = lineRenderer.GetPosition(0);
+            r_color = colorAccToPosition.GetCurrentColor();
 
-        //if locationBar is over this sprite and the marker is not moving, make it glow
-        if (lr_pos.x >= (this.transform.position.x - spriteSize.x / 2) && lr_pos.x <= (this.transform.position.x + spriteSize.x / 2)
-            && fiducial.MovementDirection == new Vector2(0.0f, 0.0f))
-        {
-            if (color.Equals('r'))
-                rend.material = redMat;
-            else if (color.Equals('g'))
-                rend.material = greenMat;
-            else
-                rend.material = blueMat;
-        }
-        //sets material to defaultMaterial and color to the initial color
-        else if (rend.material != defaultMat)
-        {
-            rend.material = defaultMat;
-            rend.color = r_color;
+            //if current_location_bar is over this sprite and the marker is not moving, make it glow
+            if (lr_pos.x >= (this.transform.position.x - spriteSize.x / 2) && lr_pos.x <= (this.transform.position.x + spriteSize.x / 2)
+                && fiducial.MovementDirection == new Vector2(0.0f, 0.0f))
+            {
+                if (r_color.r > r_color.b && r_color.r > r_color.g)
+                    rend.material = redMaterial;
+                else if (r_color.g > r_color.b && r_color.g > r_color.r)
+                    rend.material = greenMaterial;
+                else
+                    rend.material = blueMaterial;
+            }
+            //sets material to defaultMaterial if current_location_bar is not over this sprite
+            else if (rend.material != defaultMaterial)
+                rend.material = defaultMaterial;
         }
 
     }
