14e36ca1104883edb1e52094a9231dce128e1134
Raphael Kamper <dev@raphiniert.eu>
2018-09-07 12:03:20 +0200

    created BPM Manager

---
+++ Assets/Scripts/BpmManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BpmManager : MonoBehaviour {
+
+    private Settings m_settings;
+    private int bpm;
+
+	// Use this for initialization
+	void Start () {
+        m_settings = Settings.Instance;
+        bpm = m_settings.bpm;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+}
