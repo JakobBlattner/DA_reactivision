6cbc16e2bbe615fe024d18f5f4f27c1ea564d0c0
JakobBlattner <Jakoblattner@gmail.com>
2018-09-25 12:34:00 +0200

    fixed stuff

--- Assets/Scripts/LastComeLastServe.cs
+++ Assets/Scripts/LastComeLastServe.cs
@@ -166,6 +166,7 @@ public class LastComeLastServe : MonoBehaviour, TuioListener
             {
                 //gets beat on which the the marker lies on
                 int beat = m_tokenPosition.GetTactPosition(marker.transform.position);
+                //Debug.Log("beat " + beat);
                 //gets tune on which the marker lies on and checks if the tune has changed. If so --> log message
                 int tune = m_tokenPosition.GetNote(marker.transform.position);
                 if (tune != currentTunes[m_fiducial.MarkerID])
@@ -174,11 +175,13 @@ public class LastComeLastServe : MonoBehaviour, TuioListener
                     currentTunes[m_fiducial.MarkerID] = tune;
                 }
 
+                int width = (int)(m_settings.GetMarkerWidthMultiplier(m_fiducial.MarkerID) * 2);
+                if (width == 2 || width == 4)
+                    beat++;
+
                 //checks if this marker isn't already the one in the activeMarker list
                 if (activeMarkersArray[beat] != marker)
                 {
-                    int width = (int)(m_settings.GetMarkerWidthMultiplier(m_fiducial.MarkerID) * 2);
-
                     //if beat position is empty or if position is not empty, current marker hast snapped before marker on beat 
                     if (activeMarkersArray[beat] == null || (activeMarkersArray[beat].GetComponent<FiducialController>().GetLastTimeSnapped() < m_fiducial.GetLastTimeSnapped()))
                     {
@@ -186,7 +189,7 @@ public class LastComeLastServe : MonoBehaviour, TuioListener
                         if (width > 1) //mind 1/2
                         {
                             beat = beat == 0 ? 1 : beat;  // TODO: beat can't be at 0 for width > 1
-                            //checks if second beat of the marker is not ok, if so: break
+                                                          //checks if second beat of the marker is not ok, if so: break
                             if (beat - 1 > m_settings.beats || (activeMarkersArray[beat - 1] != null && (activeMarkersArray[beat - 1].GetComponent<FiducialController>().GetLastTimeSnapped() > m_fiducial.GetLastTimeSnapped())))//1/2
                                 break;
                             else
@@ -194,7 +197,7 @@ public class LastComeLastServe : MonoBehaviour, TuioListener
                                 if (width > 2)// mind 3/4
                                 {
                                     beat = beat == 0 ? 2 : beat;  // TODO: beat can't be at 0 for width > 1
-                                    //checks if third beat of the marker is not ok, if so: break
+                                                                  //checks if third beat of the marker is not ok, if so: break
                                     if (beat + 1 < 0 || (activeMarkersArray[beat + 1] != null && (activeMarkersArray[beat + 1].GetComponent<FiducialController>().GetLastTimeSnapped() > m_fiducial.GetLastTimeSnapped())))//3/4
                                         break;
                                     else
@@ -202,7 +205,7 @@ public class LastComeLastServe : MonoBehaviour, TuioListener
                                         if (width > 3)// 4/4
                                         {
                                             beat = beat == 0 ? 3 : beat;  // TODO: beat can't be at 0 for width > 3
-                                            //checks if fourth beat of the marker is not ok, if so: break
+                                                                          //checks if fourth beat of the marker is not ok, if so: break
                                             if (beat - 2 > m_settings.beats || (activeMarkersArray[beat - 2] != null && (activeMarkersArray[beat - 2].GetComponent<FiducialController>().GetLastTimeSnapped() > m_fiducial.GetLastTimeSnapped())))//4/4
                                                 break;
                                             this.ActivateMarkerOnBeatWithColor(marker, activeMarkersArray, beat - 2, color); //4/4
@@ -434,7 +437,7 @@ public class LastComeLastServe : MonoBehaviour, TuioListener
         return currentLocationBar.transform.position.x >= (marker.transform.position.x - spriteSize.x / 2) && currentLocationBar.transform.position.x <= (marker.transform.position.x + spriteSize.x / 2);
     }
 
-    private bool IsMarkerInActiveMarkers(GameObject marker)
+    public bool IsMarkerInActiveMarkers(GameObject marker)
     {
         if (enableChords)
         {
